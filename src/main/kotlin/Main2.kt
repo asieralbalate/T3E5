fbf6c6bb4ce40721045cb72bc32186b250e5365b
alu10191634 <alualb1634@ieselcaminas.org>
2023-10-04 19:50:46 +0200

    Final commit

--- src/main/kotlin/Main2.kt
+++ src/main/kotlin/Main2.kt
@@ -10,16 +10,12 @@ fun main() {
     val llistaTipus = Types.newParameterizedType(List::class.java, Ruta::class.java)
     val adapter: JsonAdapter<List<Ruta>> = moshi.adapter(llistaTipus)
 
-
-
     try {
         while (true) {
             val e = f.readObject () as Ruta
 
             val numPunts = e.size()
             for (i in 0 until numPunts) {
-                val llistaTipusPuntGeo = Types.newParameterizedType(List::class.java, PuntGeo::class.java)
-                val adapterPuntGeo: JsonAdapter<List<PuntGeo>> = moshi.adapter(llistaTipusPuntGeo)
 
                 llistaDePunts.add(PuntGeo(e.getPuntNom(i),Coordenades(e.getPuntLatitud(i),e.getPuntLongitud(i))))
             }
